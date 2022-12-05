9189b8a0e6d97a57792081048d54777d39be17f2
Dylan-Bellamy <90278426+Dylan-Bellamy@users.noreply.github.com>
2022-12-05 09:50:08 -0500

    Chess Pieces
    - Added Two subclasses called king and pawn
    - Made Methods for the superclass Piece
    - Worked on the subclasses logic

--- src/main/java/Piece.java
+++ src/main/java/Piece.java
@@ -1,6 +1,38 @@
 
-public class Piece {
-    public static void main(String[] args) {
-        
+public abstract class Piece {
+
+    /**
+     * moveValid - An abstact boolean used as a template for subclasses
+     * @param xPos - The x-coordinate of piece selected  
+     * @param yPos - The y-coordinate of piece selected  
+     * @param xMove - The x-coordinate of where the selected piece is being moved
+     * @param yMove - The y-coordinate of where the selected piece is being moved
+     * @return
+     */
+    abstract boolean moveValid(int xPos, int yPos, int xMove, int yMove);
+
+    /**
+     * horizontalVertical - A static
+     * @param xPos - The x-coordinate of piece selected  
+     * @param yPos - The y-coordinate of piece selected  
+     * @param xMove - The x-coordinate of where the selected piece is being moved
+     * @param yMove - The y-coordinate of where the selected piece is being moved
+     * @return
+     */
+    public static boolean horizontalVertical(int xPos, int yPos, int xMove, int yMove){
+        return ((xPos == xMove) || (yPos == yMove));
+    }
+
+    /**
+     * 
+     * @param xPos - The x-coordinate of piece selected  
+     * @param yPos - The y-coordinate of piece selected  
+     * @param xMove - The x-coordinate of where the selected piece is being moved
+     * @param yMove - The y-coordinate of where the selected piece is being moved
+     * @return
+     */
+    public static boolean diagonal(int xPos, int yPos, int xMove, int yMove){
+        return (Math.abs(xMove - xPos) == Math.abs(yMove - yPos));
     }
 }
+
